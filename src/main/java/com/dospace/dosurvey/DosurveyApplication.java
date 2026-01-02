9f9e688634553dfb7f5663fa2e92faa166e84894
Nguyen Cuong Thinh <ThinhNgyenCTH@gmail.com>
2026-01-02 16:08:36 +0700

    first commit

---
+++ src/main/java/com/dospace/dosurvey/DosurveyApplication.java
@@ -0,0 +1,13 @@
+package com.dospace.dosurvey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DosurveyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DosurveyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dospace/dosurvey/DosurveyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dospace.dosurvey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DosurveyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
